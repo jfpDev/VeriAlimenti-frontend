c32222774144d45dfd4123d26cbe1c4b29a452d0
jfpDev <felipeavap@hotmail.com>
2020-02-04 19:35:42 -0500

    After fixing

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/VeriAlimenti-frontend/precache-manifest.c7ea34e0aea1990a3ac32792a90dd41d.js"
+  "/VeriAlimenti-frontend/precache-manifest.b9fe6f974ea5a8fe2d8ecf387b256706.js"
 );
 
 self.addEventListener('message', (event) => {
